1559d7aaacd1bea005a1599c8b84c393968728f4
Joseph Monti <joe@42llamas.com>
2013-07-30 21:30:44 -0400

    project setup, start of monitoring dashboard

---
+++ src/main/java/org/joemonti/drogon/DrogonLoggerServlet.java
@@ -0,0 +1,42 @@
+/*
+ * Drogon : DrogonLoggerServlet.java
+ * 
+ * This file is part of Drogon.
+ *
+ * Drogon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Drogon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Drogon.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ * Author: Joseph Monti <joe.monti@gmail.com>
+ * Copyright (c) 2013 Joseph Monti All Rights Reserved, http://joemonti.org/
+ */
+
+package org.joemonti.drogon;
+
+import org.eclipse.jetty.websocket.api.UpgradeRequest;
+import org.eclipse.jetty.websocket.api.UpgradeResponse;
+import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+public class DrogonLoggerServlet extends WebSocketServlet {
+    @Override
+    public void configure(WebSocketServletFactory factory) {
+        factory.getPolicy().setIdleTimeout(10000);
+        factory.setCreator(new WebSocketCreator() {
+                @Override
+                public Object createWebSocket(UpgradeRequest req, UpgradeResponse resp) {
+                    return new DrogonLoggerSocket();
+                }
+            });
+    }
+}
---
+++ src/main/java/org/joemonti/drogon/DrogonLoggerSocket.java
@@ -0,0 +1,104 @@
+/*
+ * Drogon : DrogonLoggerServlet.java
+ * 
+ * This file is part of Drogon.
+ *
+ * Drogon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Drogon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Drogon.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ * Author: Joseph Monti <joe.monti@gmail.com>
+ * Copyright (c) 2013 Joseph Monti All Rights Reserved, http://joemonti.org/
+ */
+
+package org.joemonti.drogon;
+
+import java.io.IOException;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketAdapter;
+import org.eclipse.jetty.websocket.api.RemoteEndpoint;
+
+public class DrogonLoggerSocket extends WebSocketAdapter {
+    private Thread t;
+    
+    @Override
+    public void onWebSocketConnect(Session session) {
+        super.onWebSocketConnect( session );
+        
+        System.out.println("Client " + session.getRemoteAddress() + " Connected");
+        t = new Thread( new DrogonLoggerRunner() );
+        t.start();
+    }
+    
+    @Override
+    public void onWebSocketError(Throwable cause) {
+        cause.printStackTrace(System.err);
+    }
+    
+    @Override
+    public void onWebSocketClose(int statusCode, String reason) {
+        System.out.println("Client " + getSession().getRemoteAddress() + " Disconnected [" + statusCode + "] : " + reason);
+        super.onWebSocketClose( statusCode, reason );
+    }
+    
+    class DrogonLoggerRunner implements Runnable {
+        public void run() {
+            RemoteEndpoint remote = getSession().getRemote();
+            
+            final int VALUES = 10;
+            ValueTracker values[] = new ValueTracker[VALUES];
+            for ( int i = 0; i < VALUES; i++ ) {
+                values[i] = new ValueTracker();
+            }
+             
+            while ( isConnected() ) {
+                StringBuilder sb = new StringBuilder();
+                for ( int i = 0; i < VALUES; i++ ) {
+                    values[i].update();
+                    if ( i > 0 ) sb.append(",");
+                    sb.append( String.format("%.4f", values[i].value) );
+                }
+                
+                try {
+                    remote.sendString( sb.toString() );
+                } catch ( IOException ex ) {
+                    ex.printStackTrace( System.err );
+                    return;
+                }
+                
+                try {
+                    Thread.sleep(100);
+                } catch ( InterruptedException ex ) {
+                    System.err.print("Interrupted!!\n");
+                    return;
+                }
+            }
+        }
+    }
+    
+    static class ValueTracker {
+        private double value = 0.0;
+        double lastValue = 0.0;
+        public void update() {
+            double inc = Math.random() * 10;
+            if ( value < lastValue ) {
+                inc *= -1;
+            }
+            if ( ( ( ( value < lastValue ) && value < 0 ) || ( ( value > lastValue ) && value > 0 ) ) && 
+                    Math.random() <= Math.abs( ( value + inc ) / 100.0 ) ) {
+                inc *= -1;
+            }
+            lastValue = value;
+            value += inc;
+        }
+    }
+}
